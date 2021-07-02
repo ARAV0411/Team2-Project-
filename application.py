7d30a091ebc03e255e384cace48228b38ae2707c
ARAVINDHAN R <58031492+ARAV0411@users.noreply.github.com>
2021-07-02 18:14:11 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,27 +1,27 @@
+# Backend Python part 
+#import neccesscary module 
 from types import MemberDescriptorType
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-import sqlite3
 import cs50 
 from cs50 import SQL
 from flask import g
 import sqlalchemy
-
+# setting up flask 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 
 db = SQL("sqlite:///user.db")
-#database = sqlite3.connect("user.db",check_same_thread=False)  # part to check
-#db=database.cursor()    
-#db.execute("create table users( email_id VARCHAR(20) , hash VARCHAR(30) , mobile_no VARCHAR(10))")
 
-#db.execute(" create table VACCINE(fname varchar(20),lname varchar(20),age int ,mobile_no varchar(20),email varchar(20),country varchar(20),vaccinestatus varchar(20),vaccinetype varchar(20))")
+#Creating Table to store the data in user database    
+db.execute("create table  if not exists users( email_id VARCHAR(20) , hash VARCHAR(30) , mobile_no VARCHAR(10))")
+db.execute(" create table if not exists VACCINE(fname varchar(20),lname varchar(20),age int ,mobile_no varchar(20),email varchar(20),country varchar(20),vaccinestatus varchar(20),vaccinetype varchar(20))")
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """Log user in"""
+    """Log user in to fill the form if the user is admin ,he can see the database """
 
     # Forget any user_id
     session.clear()
@@ -30,7 +30,7 @@ def login():
     if request.method == "POST":        
             # Query database for username
             rows = db.execute("SELECT * FROM users WHERE email_id = ?",request.form.get("Email_Id"))
-                        # Ensure username exists and password is correct
+             #Ensure username exists and password is correct
             if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
                 return apology("Enter valid username/password",400 )
             # Remember which user has logged in
@@ -55,12 +55,14 @@ def logout():
     # Forget any user_id
     session.clear()
 
-    # Redirect user to login form
+    # Redirect user to home 
     return redirect("index.html")
 
 @app.route("/admin",methods=["GET","POST"])
 def admin():
+    #pre-defined admin username and password 
     if request.method == "POST":
+        # for refresh website 
         if request.form.get("sub"):
             redirect("/admin")
     else:
@@ -69,35 +71,26 @@ def admin():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register user"""
+    """Register user and save the password using hash function"""
     if request.method == "POST":
-
         mobile = request.form.get("mobile_no")
-        
         if len(mobile)!=10:
             return apology("Enter correct mobile number",400)
-
         email = str(request.form.get("Email"))
-        
-        
+        #checking the email id have 314ecorp domain
         listofemail= email.split("@")
-        
         if listofemail[1] != "314ecorp.com":
             return apology("Enter the official mail id",400)
         p1=request.form.get("password")
         p2=request.form.get("confirm_password")
         if p1!=p2 :
             return apology("password mismatch",403)
- 
-
         password = generate_password_hash(request.form.get("password"))
         result = db.execute("INSERT INTO users(email_id,hash,mobile_no) VALUES(?,?,?)",
                           email,password,mobile)
-#        result = db.execute("INSERT INTO users(email_id) VALUES(?)",
-#                           (email))
+        #if the Email id already exists
         if not result:
             return apology("email already exists", 400)
-
         session["user_id"] = result
         """Success message"""
         flash('Registered! Sign in using the same email id')
@@ -107,6 +100,7 @@ def register():
 
 @app.route("/form",method=["Get","Post"])
 def form():
+    """ The form that collect the vaccination data """
      if request.method == "POST":
         "function for checking vaccinate"
         firstname=request.form.get("fname")
@@ -120,22 +114,18 @@ def form():
         print(firstname,lastname,age,mobile_no,email,country,vaccine_status,vaccine_administered)
         db.execute("INSERT INTO VACCINE(fname,lname,age,mobile_no,email,country,vaccinestatus,vaccinetype) VALUES(?,?,?,?,?,?,?,?)",firstname,lastname,age,mobile_no
                 ,email,country,vaccine_status,vaccine_administered)
-
         if vaccine_status=="0-dose":
             return redirect("https://www.cowin.gov.in/home")
         elif vaccine_status=="1-dose":
              return redirect("https://www.cowin.gov.in/home")
-        else:
+        if (vaccine_status=="2-dose" or vaccine_administered=="sputnikv"):
              return  redirect("https://www.amazon.in/gift-card-store/b?ie=UTF8&node=3704982031")
         
-        
+     # for get method 
      else:
        return(render_template("form.html")) 
 
-
-
-
-  # function for returning apology message 
+  # function for returning apology message using html
 def apology(message,error_id):
 #    return("Error"+" "+message)
      return(render_template("apology.html",top=message ))
@@ -143,6 +133,7 @@ def apology(message,error_id):
 
 @app.route('/')
 def home():
+    """Root function"""
    return render_template('index.html')
 if __name__ == '__main__':
    app.run()
